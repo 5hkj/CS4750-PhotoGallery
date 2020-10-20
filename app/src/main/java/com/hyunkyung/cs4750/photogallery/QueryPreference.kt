e1cdaf41adc314565df4790a5697a6921f5e18b5
5hkj <hyun-kyung@outlook.com>
2020-10-20 14:44:04 -0700

    Chapter 27

--- app/src/main/java/com/hyunkyung/cs4750/photogallery/QueryPreference.kt
+++ app/src/main/java/com/hyunkyung/cs4750/photogallery/QueryPreference.kt
@@ -6,6 +6,7 @@ import androidx.core.content.edit
 
 private const val PREF_SEARCH_KEY = "searchQuery"
 private const val PREF_LAST_RESULT_ID = "lastResultId"
+private const val PREF_IS_POLLING = "isPolling"
 
 object QueryPreference {
 
@@ -32,4 +33,14 @@ object QueryPreference {
         }
     }
 
+    fun isPolling(context: Context): Boolean {
+        return PreferenceManager.getDefaultSharedPreferences(context)
+            .getBoolean(PREF_IS_POLLING, false)
+    }
+
+    fun setPolling(context: Context, isOn: Boolean) {
+        PreferenceManager.getDefaultSharedPreferences(context).edit {
+            putBoolean(PREF_IS_POLLING, isOn)
+        }
+    }
 }
